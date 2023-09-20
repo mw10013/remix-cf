81cf0b8f694374705bdc14743bab97edb61cbda8
Michael Wu <mw10013@gmail.com>
2023-09-20 18:03:25 -0400

    add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
 ];
 
 export default function App() {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="p-8">
+      <h1 className="text-lg font-semibold">Welcome to Remix</h1>
       <ul>
         <li>
           <a
