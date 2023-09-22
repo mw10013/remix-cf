999d5f2772e0bf0f32ba2177645ad39b8bd8e1fd
Michael Wu <mw10013@gmail.com>
2023-09-22 19:26:56 -0400

    add openai

---
+++ app/routes/openai.tsx
@@ -0,0 +1,34 @@
+import { useChat } from "ai/react";
+
+export default function Route() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/openai",
+  });
+
+  return (
+    <main className="mx-auto flex h-screen w-full max-w-lg flex-col p-24">
+      <section className="m mb-auto">
+        {messages.map((m) => (
+          <div className="mb-4" key={m.id}>
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
+      </section>
+      <form className="flex space-x-4" onSubmit={handleSubmit}>
+        <input
+          className="rounded-md p-2 text-black"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Say something..."
+        />
+        <button
+          className="rounded-md border-2 border-solid border-white p-2"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </main>
+  );
+}
