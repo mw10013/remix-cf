3ce414bfe93a5518f0d01acf5f0adbffd4870b44
Michael Wu <mw10013@gmail.com>
2023-09-28 17:40:53 -0400

    wip

--- app/routes/autoscroll.tsx
+++ app/routes/autoscroll.tsx
@@ -1,38 +1,46 @@
 import * as React from "react";
 import { faker } from "@faker-js/faker";
 import { Button, Card, CardBody } from "@nextui-org/react";
-import {
-  useIntersectionObserver,
-  useMeasure,
-  useWindowScroll,
-} from "@uidotdev/usehooks";
+import { useMeasure } from "@uidotdev/usehooks";
 
 export default function Route() {
   const [text, setText] = React.useState("");
   // const rootRef = React.useRef<HTMLDivElement>(null);
-  const [intersectionRef, entry] = useIntersectionObserver<HTMLDivElement>({
-    // root: rootRef.current,
-    root: null,
-    rootMargin: "0px",
-    threshold: 1,
-  });
-  const [measureRef, { width, height }] = useMeasure<HTMLDivElement>();
-  const [{ x, y }] = useWindowScroll();
+  // const [intersectionRef, entry] = useIntersectionObserver<HTMLDivElement>({
+  //   // root: rootRef.current,
+  //   root: null,
+  //   rootMargin: "0px",
+  //   threshold: 1,
+  // });
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const [contentRef, { width, height }] = useMeasure<HTMLDivElement>();
+
+  React.useEffect(() => {
+    if (containerRef.current === null) return;
+    const { clientHeight, offsetHeight, scrollHeight, scrollTop } =
+      containerRef.current;
+    console.log("containerRef:", {
+      scrollHeight,
+      scrollTop,
+      offsetHeight,
+      clientHeight,
+      sum: scrollTop + offsetHeight + 100,
+    });
+    if (scrollHeight <= scrollTop + offsetHeight + 100) {
+      console.log("scrollTo:");
+      containerRef.current.scrollTo(0, scrollHeight);
+    }
+  }, [containerRef, text]);
   return (
     <>
       <Card className="m-8 mx-auto max-w-md">
         <CardBody id="cardBody" className="flex h-64 flex-col">
           <div
-            id="scrollingElement"
+            ref={containerRef}
             className="max-h-48 flex-grow overflow-y-auto border-1 border-blue-700"
           >
-            <div id="content" className="m-2 border-1 border-orange-600">
-              <div ref={measureRef}>{text}</div>
-              <div
-                id="purpleDiv"
-                ref={intersectionRef}
-                className="h-4 bg-purple-600"
-              ></div>
+            <div ref={contentRef} className="m-2 border-1 border-orange-600">
+              {text}
             </div>
           </div>
           <div className="mt-2 h-4 bg-slate-500"></div>
@@ -53,15 +61,13 @@ export default function Route() {
           >
             Lorem
           </Button>
-          <div>isIntersecting: {entry?.isIntersecting.toString()}</div>
+          {/* <div>isIntersecting: {entry?.isIntersecting.toString()}</div>
           <div>intersectionRatio: {entry?.intersectionRatio}</div>
           <div>target.tagName: {entry?.target.tagName}</div>
           <div>target.className: {entry?.target.className}</div>
-          <div>target.id: {entry?.target.id}</div>
+          <div>target.id: {entry?.target.id}</div> */}
           <div>width: {width}</div>
           <div>height: {height}</div>
-          <div>x: {x}</div>
-          <div>y: {y}</div>
         </CardBody>
       </Card>
     </>
