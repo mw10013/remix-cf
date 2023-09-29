cbb9e423bb5c86a3b7133fabfc795e9c44e37b13
Michael Wu <mw10013@gmail.com>
2023-09-29 19:00:09 -0400

    closes #2 autoscroll

--- app/routes/autoscroll.tsx
+++ app/routes/autoscroll.tsx
@@ -1,25 +1,15 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { Button, Card, CardBody } from "@nextui-org/react";
 
 export default function Route() {
   const [text, setText] = useState("");
 
-  // https://dev.to/martinez/real-scroll-for-a-chat-application-22co
-  const containerRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (containerRef.current === null) return;
-    const { offsetHeight, scrollHeight, scrollTop } = containerRef.current;
-    if (scrollHeight <= scrollTop + offsetHeight + 100) {
-      containerRef.current.scrollTo(0, scrollHeight);
-    }
-  }, [containerRef, text]);
-
   return (
     <Card className="m-8 mx-auto max-w-md">
       <CardBody id="cardBody" className="flex h-64 flex-col gap-2">
         <div
-          ref={containerRef}
-          className="h-48 overflow-y-auto border-1 border-blue-700 p-2"
+          // https://code.hnldesign.nl/scrolltobottom/
+          className="flex h-48 flex-col-reverse overflow-y-auto border-1 border-blue-700 p-2"
         >
           {text}
         </div>
