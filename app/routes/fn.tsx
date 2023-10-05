2c17c1f5fe0aab3bbc6b4e57a6e1dc059f0b4b50
Michael Wu <mw10013@gmail.com>
2023-10-05 16:46:13 -0400

    generateDatabaseRecord

--- app/routes/fn.tsx
+++ app/routes/fn.tsx
@@ -1,6 +1,7 @@
 import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
 import { ActionFunctionArgs } from "@remix-run/cloudflare";
 import { Form, useActionData } from "@remix-run/react";
+import { createStructuredOutputChainFromZod } from "langchain/chains/openai_functions";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 import {
@@ -12,33 +13,33 @@ import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import { assertCloudflareEnv } from "~/types/cloudflareEnv";
 
-const zodSchema = z.object({
-  foods: z
-    .array(
-      z.object({
-        name: z.string().describe("The name of the food item"),
-        healthy: z.boolean().describe("Whether the food is good for you"),
-        color: z.string().optional().describe("The color of the food"),
-      }),
-    )
-    .describe("An array of food items mentioned in the text"),
-});
+// https://js.langchain.com/docs/modules/chains/popular/structured_output
 
-const prompt = new ChatPromptTemplate({
-  promptMessages: [
-    SystemMessagePromptTemplate.fromTemplate(
-      "List all food items mentioned in the following text.",
-    ),
-    HumanMessagePromptTemplate.fromTemplate("{inputText}"),
-  ],
-  inputVariables: ["inputText"],
-});
+async function formatTextIntoStructuredData(openAIApiKey: string) {
+  const zodSchema = z.object({
+    foods: z
+      .array(
+        z.object({
+          name: z.string().describe("The name of the food item"),
+          healthy: z.boolean().describe("Whether the food is good for you"),
+          color: z.string().optional().describe("The color of the food"),
+        }),
+      )
+      .describe("An array of food items mentioned in the text"),
+  });
 
-export async function action({ context }: ActionFunctionArgs) {
-  assertCloudflareEnv(context.env);
+  const prompt = new ChatPromptTemplate({
+    promptMessages: [
+      SystemMessagePromptTemplate.fromTemplate(
+        "List all food items mentioned in the following text.",
+      ),
+      HumanMessagePromptTemplate.fromTemplate("{inputText}"),
+    ],
+    inputVariables: ["inputText"],
+  });
   const llm = new ChatOpenAI({
     modelName: "gpt-3.5-turbo-0613",
-    openAIApiKey: context.env.OPENAI_API_KEY,
+    openAIApiKey,
     temperature: 0,
   });
 
@@ -57,8 +58,65 @@ export async function action({ context }: ActionFunctionArgs) {
   const response = await chain.invoke({
     inputText: "I like apples, bananas, oxygen, and french fries.",
   });
-  console.log("fn: action: response:", JSON.stringify(response, null, 2));
-  return { data: response };
+  return response;
+}
+
+async function generateDatabaseRecord(openAIApiKey: string) {
+  const zodSchema = z.object({
+    name: z.string().describe("Human name"),
+    surname: z.string().describe("Human surname"),
+    age: z.number().describe("Human age"),
+    birthplace: z.string().describe("Where the human was born"),
+    appearance: z.string().describe("Human appearance description"),
+    shortBio: z.string().describe("Short bio secription"),
+    university: z.string().optional().describe("University name if attended"),
+    gender: z.string().describe("Gender of the human"),
+    interests: z
+      .array(z.string())
+      .describe("json array of strings human interests"),
+  });
+
+  const prompt = new ChatPromptTemplate({
+    promptMessages: [
+      SystemMessagePromptTemplate.fromTemplate(
+        "Generate details of a hypothetical person.",
+      ),
+      HumanMessagePromptTemplate.fromTemplate(
+        "Additional context: {inputText}",
+      ),
+    ],
+    inputVariables: ["inputText"],
+  });
+
+  const llm = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo-0613",
+    openAIApiKey,
+    temperature: 1,
+  });
+
+  const chain = createStructuredOutputChainFromZod(zodSchema, {
+    prompt,
+    llm,
+    outputKey: "person",
+  });
+
+  const response = await chain.call({
+    inputText:
+      "Please generate a diverse group of people, but don't generate anyone who likes video games.",
+  });
+  return response;
+}
+
+export async function action({ context }: ActionFunctionArgs) {
+  assertCloudflareEnv(context.env);
+  return {
+    formatTextIntoStructuredData: await formatTextIntoStructuredData(
+      context.env.OPENAI_API_KEY,
+    ),
+    generateDatabaseRecord: await generateDatabaseRecord(
+      context.env.OPENAI_API_KEY,
+    ),
+  };
 }
 
 export default function Route() {
