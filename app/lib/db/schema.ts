cdb5a3e1556d74b423e0ef7f917b51f60f996266
Michael Wu <mw10013@gmail.com>
2023-10-14 10:22:07 -0400

    chatMessages

--- app/lib/db/schema.ts
+++ app/lib/db/schema.ts
@@ -1,13 +1,62 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const chats = sqliteTable("chats", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   createdAt: text("createdAt").default(sql`CURRENT_TIMESTAMP`),
 });
 
+/**
+ * Must align with CloudflareD1MessageHistory schema in ensureTable() method.
+ */
+export const chatMessages = sqliteTable(
+  "chatMessages",
+  {
+    id: text("id").primaryKey(),
+    sessionId: text("sessionId")
+      .notNull()
+      .references(() => chats.id),
+    type: text("type").notNull(),
+    content: text("content").notNull(),
+    role: text("role"),
+    name: text("name"),
+    additional_kwargs: text("additional_kwargs").notNull(),
+  },
+  (table) => {
+    return {
+      sessionIdIndex: index("sessionIdIndex").on(table.sessionId),
+    };
+  },
+);
+
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
   comment: text("comment"),
 });
+
+// interface selectStoredMessagesDTO {
+//   id: string;
+//   session_id: string;
+//   type: string;
+//   content: string;
+//   role: string | null;
+//   name: string | null;
+//   additional_kwargs: string;
+
+// private async ensureTable(): Promise<void> {
+//   if (this.tableInitialized) {
+//     return;
+//   }
+
+//   const query = `CREATE TABLE IF NOT EXISTS ${this.tableName} (id TEXT PRIMARY KEY, session_id TEXT, type TEXT, content TEXT, role TEXT, name TEXT, additional_kwargs TEXT);`;
+//   await this.database.prepare(query).bind().all();
+
+//   const idIndexQuery = `CREATE INDEX IF NOT EXISTS id_index ON ${this.tableName} (id);`;
+//   await this.database.prepare(idIndexQuery).bind().all();
+
+//   const sessionIdIndexQuery = `CREATE INDEX IF NOT EXISTS session_id_index ON ${this.tableName} (session_id);`;
+//   await this.database.prepare(sessionIdIndexQuery).bind().all();
+
+//   this.tableInitialized = true;
+// }
