5fe7c44f38368b93601f134864a1b0e3b246def1
Michael Wu <mw10013@gmail.com>
2023-10-20 11:14:59 -0400

    redox access token

--- scripts/achat.ts
+++ scripts/achat.ts
@@ -1,12 +1,109 @@
 import readline from "readline/promises";
+import * as jose from "jose";
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { BufferMemory } from "langchain/memory";
 import { RunnableSequence } from "langchain/schema/runnable";
 import { DynamicStructuredTool, DynamicTool } from "langchain/tools";
+import invariant from "tiny-invariant";
 import { z } from "zod";
 
 console.log("achat");
+
+const AUD = "https://api.redoxengine.com/v2/auth/token";
+
+async function assertResponseOk(res: Response) {
+  if (!res.ok) {
+    const error = new Error(
+      `${res.status} ${res.statusText} ${await res.text()}`,
+    );
+    throw error;
+  }
+}
+
+async function requestJwtAccessToken(signedAssertion: string, scope: string) {
+  const requestBody = new URLSearchParams();
+  requestBody.append("grant_type", "client_credentials");
+  requestBody.append(
+    "client_assertion_type",
+    "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+  );
+  requestBody.append("client_assertion", signedAssertion);
+  requestBody.append("scope", scope);
+
+  const response = await fetch("https://api.redoxengine.com/v2/auth/token", {
+    method: "POST",
+    body: requestBody,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  await assertResponseOk(response);
+  return await response.json<{
+    access_token: string;
+    scope: string;
+    token_type: string;
+    expires_in: number;
+  }>();
+}
+
+async function getSignedAssertion({
+  privateKeyJwk,
+  clientId,
+  scope,
+  kid,
+  aud,
+}: {
+  privateKeyJwk: jose.JWK;
+  clientId: string;
+  scope: string;
+  kid: string;
+  aud: string;
+}) {
+  const privateKey = await jose.importJWK(privateKeyJwk, "RS384");
+  const payload = {
+    scope,
+  };
+
+  const randomBytes = new Uint8Array(8);
+  crypto.getRandomValues(randomBytes);
+  const signedAssertion = await new jose.SignJWT(payload)
+    .setProtectedHeader({
+      alg: "RS384",
+      kid,
+    })
+    .setAudience(aud)
+    .setIssuer(clientId)
+    .setSubject(clientId)
+    .setIssuedAt(Math.floor(new Date().getTime() / 1000)) // Current timestamp in seconds (undefined is valid)
+    .setJti(
+      Array.from(randomBytes) // Array.from() so that map returns string whereas Uint8Array.map returns number.
+        .map((byte) => byte.toString(16).padStart(2, "0"))
+        .join(""),
+    ) // a random string to prevent replay attacks
+    .sign(privateKey);
+  return signedAssertion;
+}
+
+invariant(
+  typeof process.env.REDOX_API_PRIVATE_JWK === "string" &&
+    typeof process.env.REDOX_API_CLIENT_ID === "string" &&
+    typeof process.env.REDOX_API_SCOPE === "string" &&
+    typeof process.env.REDOX_API_PUBLIC_KID === "string",
+  "Invalid env",
+);
+const signedAssertion = await getSignedAssertion({
+  privateKeyJwk: JSON.parse(process.env.REDOX_API_PRIVATE_JWK) as jose.JWK,
+  clientId: process.env.REDOX_API_CLIENT_ID,
+  scope: process.env.REDOX_API_SCOPE,
+  kid: process.env.REDOX_API_PUBLIC_KID,
+  aud: AUD,
+});
+const jwtAccessToken = await requestJwtAccessToken(
+  signedAssertion,
+  process.env.REDOX_API_SCOPE,
+);
+console.log("jwtAccessToken:", jwtAccessToken);
 
 const tools = [
   new DynamicTool({
