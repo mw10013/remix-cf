bee5137dd14e0681380e6a0fb485aa9be1bdd14c
Michael Wu <mw10013@gmail.com>
2023-10-23 15:24:54 -0400

    redox object

--- scripts/bchat.ts
+++ scripts/bchat.ts
@@ -8,95 +8,128 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 
 console.log("bchat");
 
-const AUD = "https://api.redoxengine.com/v2/auth/token";
+async function createRedox() {
+  const endpoint =
+    "https://api.redoxengine.com/fhir/R4/redox-fhir-sandbox/Development/";
+  const aud = "https://api.redoxengine.com/v2/auth/token";
 
-async function assertResponseOk(res: Response) {
-  if (!res.ok) {
-    const error = new Error(
-      `${res.status} ${res.statusText} ${await res.text()}`,
-    );
-    throw error;
-  }
-}
-
-async function requestJwtAccessToken(signedAssertion: string, scope: string) {
-  const requestBody = new URLSearchParams();
-  requestBody.append("grant_type", "client_credentials");
-  requestBody.append(
-    "client_assertion_type",
-    "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+  invariant(
+    typeof process.env.REDOX_API_PRIVATE_JWK === "string" &&
+      typeof process.env.REDOX_API_CLIENT_ID === "string" &&
+      typeof process.env.REDOX_API_SCOPE === "string" &&
+      typeof process.env.REDOX_API_PUBLIC_KID === "string",
+    "Invalid env",
   );
-  requestBody.append("client_assertion", signedAssertion);
-  requestBody.append("scope", scope);
-
-  const response = await fetch("https://api.redoxengine.com/v2/auth/token", {
-    method: "POST",
-    body: requestBody,
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
+  const signedAssertion = await getSignedAssertion({
+    privateKeyJwk: JSON.parse(process.env.REDOX_API_PRIVATE_JWK) as jose.JWK,
+    clientId: process.env.REDOX_API_CLIENT_ID,
+    scope: process.env.REDOX_API_SCOPE,
+    kid: process.env.REDOX_API_PUBLIC_KID,
+    aud,
   });
-  await assertResponseOk(response);
-  return await response.json<{
-    access_token: string;
-    scope: string;
-    token_type: string;
-    expires_in: number;
-  }>();
-}
 
-async function getSignedAssertion({
-  privateKeyJwk,
-  clientId,
-  scope,
-  kid,
-  aud,
-}: {
-  privateKeyJwk: jose.JWK;
-  clientId: string;
-  scope: string;
-  kid: string;
-  aud: string;
-}) {
-  const privateKey = await jose.importJWK(privateKeyJwk, "RS384");
-  const payload = {
-    scope,
+  const redox = {
+    post,
   };
 
-  const randomBytes = new Uint8Array(8);
-  crypto.getRandomValues(randomBytes);
-  const signedAssertion = await new jose.SignJWT(payload)
-    .setProtectedHeader({
-      alg: "RS384",
-      kid,
-    })
-    .setAudience(aud)
-    .setIssuer(clientId)
-    .setSubject(clientId)
-    .setIssuedAt(Math.floor(new Date().getTime() / 1000)) // Current timestamp in seconds (undefined is valid)
-    .setJti(
-      Array.from(randomBytes) // Array.from() so that map returns string whereas Uint8Array.map returns number.
-        .map((byte) => byte.toString(16).padStart(2, "0"))
-        .join(""),
-    ) // a random string to prevent replay attacks
-    .sign(privateKey);
-  return signedAssertion;
-}
+  async function post(api: string, body: unknown) {
+    invariant(
+      typeof process.env.REDOX_API_SCOPE === "string",
+      "Invalid REDOX_API_SCOPE",
+    );
+    const jwtAccessToken = await requestJwtAccessToken(
+      signedAssertion,
+      process.env.REDOX_API_SCOPE,
+    );
+    console.log("jwtAccessToken:", jwtAccessToken);
 
-invariant(
-  typeof process.env.REDOX_API_PRIVATE_JWK === "string" &&
-    typeof process.env.REDOX_API_CLIENT_ID === "string" &&
-    typeof process.env.REDOX_API_SCOPE === "string" &&
-    typeof process.env.REDOX_API_PUBLIC_KID === "string",
-  "Invalid env",
-);
-const signedAssertion = await getSignedAssertion({
-  privateKeyJwk: JSON.parse(process.env.REDOX_API_PRIVATE_JWK) as jose.JWK,
-  clientId: process.env.REDOX_API_CLIENT_ID,
-  scope: process.env.REDOX_API_SCOPE,
-  kid: process.env.REDOX_API_PUBLIC_KID,
-  aud: AUD,
-});
+    const response = await fetch(endpoint + api, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${jwtAccessToken.access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    await assertResponseOk(response);
+    return await response.text();
+  }
+
+  async function assertResponseOk(res: Response) {
+    if (!res.ok) {
+      const error = new Error(
+        `${res.status} ${res.statusText} ${await res.text()}`,
+      );
+      throw error;
+    }
+  }
+
+  async function requestJwtAccessToken(signedAssertion: string, scope: string) {
+    const requestBody = new URLSearchParams();
+    requestBody.append("grant_type", "client_credentials");
+    requestBody.append(
+      "client_assertion_type",
+      "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+    );
+    requestBody.append("client_assertion", signedAssertion);
+    requestBody.append("scope", scope);
+
+    const response = await fetch("https://api.redoxengine.com/v2/auth/token", {
+      method: "POST",
+      body: requestBody,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    await assertResponseOk(response);
+    return await response.json<{
+      access_token: string;
+      scope: string;
+      token_type: string;
+      expires_in: number;
+    }>();
+  }
+
+  async function getSignedAssertion({
+    privateKeyJwk,
+    clientId,
+    scope,
+    kid,
+    aud,
+  }: {
+    privateKeyJwk: jose.JWK;
+    clientId: string;
+    scope: string;
+    kid: string;
+    aud: string;
+  }) {
+    const privateKey = await jose.importJWK(privateKeyJwk, "RS384");
+    const payload = {
+      scope,
+    };
+
+    const randomBytes = new Uint8Array(8);
+    crypto.getRandomValues(randomBytes);
+    const signedAssertion = await new jose.SignJWT(payload)
+      .setProtectedHeader({
+        alg: "RS384",
+        kid,
+      })
+      .setAudience(aud)
+      .setIssuer(clientId)
+      .setSubject(clientId)
+      .setIssuedAt(Math.floor(new Date().getTime() / 1000)) // Current timestamp in seconds (undefined is valid)
+      .setJti(
+        Array.from(randomBytes) // Array.from() so that map returns string whereas Uint8Array.map returns number.
+          .map((byte) => byte.toString(16).padStart(2, "0"))
+          .join(""),
+      ) // a random string to prevent replay attacks
+      .sign(privateKey);
+    return signedAssertion;
+  }
+
+  return redox;
+}
 
 type FunctionDescription<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -108,6 +141,7 @@ type FunctionDescription<
   func: (input: z.infer<T>) => Promise<string>;
 };
 
+const redox = await createRedox();
 const functionDescriptions: FunctionDescription[] = [
   {
     name: "getCurrentWeather",
@@ -135,33 +169,11 @@ const functionDescriptions: FunctionDescription[] = [
     description: "Get Keva Green's details",
     schema: z.object({}),
     func: async () => {
-      invariant(
-        typeof process.env.REDOX_API_SCOPE === "string",
-        "Invalid REDOX_API_SCOPE",
-      );
-      const jwtAccessToken = await requestJwtAccessToken(
-        signedAssertion,
-        process.env.REDOX_API_SCOPE,
-      );
-      console.log("jwtAccessToken:", jwtAccessToken);
-
-      const response = await fetch(
-        "https://api.redoxengine.com/fhir/R4/redox-fhir-sandbox/Development/Patient/_search",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${jwtAccessToken.access_token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            given: "Keva",
-            family: "Green",
-            birthdate: "1995-08-26",
-          }),
-        },
-      );
-      await assertResponseOk(response);
-      return response.text();
+      return redox.post("Patient/_search", {
+        given: "Keva",
+        family: "Green",
+        birthdate: "1995-08-26",
+      });
     },
   },
 ];
@@ -170,15 +182,23 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const messages: ChatCompletionMessageParam[] = [
-  { role: "system", content: "You are a helpful assistant." },
-];
+function createMessageStore() {
+  const messageStore = {
+    messages: [] as ChatCompletionMessageParam[],
+    add(message: ChatCompletionMessageParam) {
+      messageStore.messages.push(message);
+    },
+  };
+  return messageStore;
+}
 
-async function completeMessages() {
+type MessageStore = ReturnType<typeof createMessageStore>;
+
+async function completeMessages(messageStore: MessageStore) {
   const completion = await openai.chat.completions.create({
     model: "gpt-4-0613",
     temperature: 0,
-    messages,
+    messages: messageStore.messages,
     functions: functionDescriptions.map(({ name, description, schema }) => {
       return {
         name,
@@ -190,7 +210,7 @@ async function completeMessages() {
   // console.log("completion:", completion);
   const completionMessage = completion.choices[0].message;
   console.log("completionMessage:", completionMessage);
-  messages.push(completionMessage);
+  messageStore.add(completionMessage);
 
   if (!completionMessage.function_call) {
     return completionMessage.content;
@@ -218,7 +238,7 @@ async function completeMessages() {
     return `Error parsing arguments for function ${functionName}: ${parseResult.error.message}`;
   }
   const functionOutput = await functionDescription.func(parseResult.data);
-  messages.push({
+  messageStore.add({
     role: "function",
     name: functionName,
     content: functionOutput,
@@ -226,13 +246,16 @@ async function completeMessages() {
   const functionCompletion = await openai.chat.completions.create({
     model: "gpt-4-0613",
     temperature: 0,
-    messages: messages,
+    messages: messageStore.messages,
   });
   const functionCompletionMessage = functionCompletion.choices[0].message;
   console.log("functionCompletionMessage:", functionCompletionMessage);
-  messages.push(functionCompletionMessage);
+  messageStore.add(functionCompletionMessage);
   return functionCompletionMessage.content;
 }
+
+const messageStore = createMessageStore();
+messageStore.add({ role: "system", content: "You are a helpful assistant." });
 
 const stdio = readline.createInterface({
   input: process.stdin,
@@ -244,8 +267,8 @@ while (true) {
   if (input.toLowerCase() === "quit" || input.toLowerCase() === "exit") {
     break;
   } else if (input.length) {
-    messages.push({ role: "user", content: input });
-    const output = await completeMessages();
+    messageStore.add({ role: "user", content: input });
+    const output = await completeMessages(messageStore);
     console.log(`🦫 > ${output}`);
   }
 }
