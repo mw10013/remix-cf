0721707a4ff25703b9fda9b07ca6d550e2dc639b
Michael Wu <mw10013@gmail.com>
2023-10-29 17:54:00 -0400

    post to data model api

--- scripts/redox_fhir.ts
+++ scripts/redox_fhir.ts
@@ -15,9 +15,8 @@ function createEnv() {
 }
 
 async function createRedox(env: ReturnType<typeof createEnv>) {
-  // const endpoint =
-  //   "https://api.redoxengine.com/fhir/R4/redox-fhir-sandbox/Development/";
-  const endpoint = "https://api.redoxengine.com/endpoint";
+  const endpoint =
+    "https://api.redoxengine.com/fhir/R4/redox-fhir-sandbox/Development/";
   const aud = "https://api.redoxengine.com/v2/auth/token";
 
   const signedAssertion = await getSignedAssertion({
@@ -50,13 +49,14 @@ async function createRedox(env: ReturnType<typeof createEnv>) {
     return await response.json();
   }
 
-  async function post(body: unknown) {
+  async function post(api: string, body: unknown) {
     const jwtAccessToken = await requestJwtAccessToken(
       signedAssertion,
       env.REDOX_API_SCOPE,
     );
+    // console.log("jwtAccessToken:", jwtAccessToken);
 
-    const response = await fetch(endpoint, {
+    const response = await fetch(endpoint + api, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${jwtAccessToken.access_token}`,
@@ -147,51 +147,94 @@ async function createRedox(env: ReturnType<typeof createEnv>) {
 const env = createEnv();
 const redox = await createRedox(env);
 
-// { ID: '0000000001', IDType: 'MR' },
-// { ID: 'e167267c-16c9-4fe3-96ae-9cff5703e90a', IDType: 'EHRID' },
-// { ID: 'a1d4ee8aba494ca', IDType: 'NIST' }
+// These may be interchangeable in many cases.
+// Inactive: const patientId = "69efd2ea-1256-4ae7-b4ec-5d0160427185";
+const patientId = "81c2f5eb-f99f-40c4-b504-59483e6148d7";
+const patientIdentifier = "Patient/81c2f5eb-f99f-40c4-b504-59483e6148d7";
 
-// https://developer.redoxengine.com/data-models/PatientSearch.html#Query
-console.log("😀> PatientSearch Query: Timothy Bixby, 2008-01-06");
+// https://dashboard.redoxengine.com/#/organization/15903/development/actions/search-for-a-patient-with-identifier
+// https://docs.redoxengine.com/fhir-api-actions/patients/search-for-a-patient-with-identifier/
+console.log(
+  "😀> Search: urn:redox:redox-fhir-sandbox:MR|kyHGADnvX3xbkU4V9ayaqh",
+);
 console.dir(
-  await redox.post({
-    Meta: {
-      DataModel: "PatientSearch",
-      EventType: "Query",
-      Destinations: [
-        {
-          ID: "0f4bd1d1-451d-4351-8cfd-b767d1b488d6",
-          Name: "Patient Search Endpoint",
-        },
-      ],
-    },
-    Patient: {
-      Demographics: {
-        FirstName: "Timothy",
-        LastName: "Bixby",
-        DOB: "2008-01-06",
-      },
-    },
+  await redox.post("Patient/_search", {
+    identifier: "urn:redox:redox-fhir-sandbox:MR|kyHGADnvX3xbkU4V9ayaqh",
   }),
   { depth: null },
 );
 
-console.log("😀> PatientSearch Query: { ID: '0000000001', IDType: 'MR' }");
+// https://dashboard.redoxengine.com/#/organization/15903/development/actions/search-patient-by-demographics
+// https://docs.redoxengine.com/fhir-api-actions/patients/search-for-a-patient-by-demographics/
+console.log("😀> Search: Keva Green 1995-08-26");
 console.dir(
-  await redox.post({
-    Meta: {
-      DataModel: "PatientSearch",
-      EventType: "Query",
-      Destinations: [
-        {
-          ID: "0f4bd1d1-451d-4351-8cfd-b767d1b488d6",
-          Name: "Patient Search Endpoint",
-        },
-      ],
-    },
-    Patient: {
-      Identifiers: [{ ID: "0000000001", IDType: "MR" }],
-    },
+  await redox.post("Patient/_search", {
+    given: "Keva",
+    family: "Green",
+    birthdate: "1995-08-26",
   }),
   { depth: null },
 );
+
+// https://docs.redoxengine.com/api-reference/fhir-api-reference/fhir-resources/Patient/read/
+console.log(`get patient: ${patientId}`);
+console.dir(await redox.get(`Patient/${patientId}`), { depth: null });
+
+// https://dashboard.redoxengine.com/#/organization/15903/development/actions/retrieve-a-patients-diagnoses
+// https://docs.redoxengine.com/fhir-api-actions/general-clinical/retrieve-a-patients-diagnoses/
+// https://docs.redoxengine.com/api-reference/fhir-api-reference/fhir-resources/Condition/_search/
+console.log(`😀> Retrieve diagnoses for ${patientIdentifier}`);
+console.dir(
+  await redox.post(`Condition/_search`, {
+    patient: patientIdentifier,
+    category: "problem-list-item",
+  }),
+  { depth: null },
+);
+
+// console.log(`😀> Retrieve diagnoses for ${patientId}`);
+console.dir(
+  await redox.post(`Condition/_search`, {
+    patient: patientId,
+    category: "problem-list-item",
+  }),
+  { depth: null },
+);
+
+// https://dashboard.redoxengine.com/#/organization/15903/development/actions/retrieve-a-patients-transition-of-care-document
+console.log(`😀> Retrieve document list for ${patientId}`);
+console.dir(
+  await redox.post(`DocumentReference/_search`, {
+    patient: patientId,
+  }),
+  { depth: null },
+);
+
+// https://docs.redoxengine.com/api-reference/fhir-api-reference/fhir-resources/DocumentReference/_search/
+const documentId = "74d7f89e-d141-444b-a967-602525f34fc2";
+console.log(
+  `😀> Retrieve a patient's transition of care document (${documentId})`,
+);
+console.dir(
+  await redox.post(`DocumentReference/_search`, {
+    _id: documentId,
+  }),
+  { depth: null },
+);
+
+// https://dashboard.redoxengine.com/#/organization/15903/development/actions/retrieve-a-patients-appointment-schedule
+// https://docs.redoxengine.com/fhir-api-actions/visit-and-scheduling/retrieve-a-patients-appointment-schedule/
+// https://docs.redoxengine.com/api-reference/fhir-api-reference/fhir-resources/Appointment/_search/
+console.log(`😀> Retrieve appointment schedule for ${patientId}`);
+console.dir(
+  await redox.post(`Appointment/_search`, {
+    patient: patientId,
+  }),
+  { depth: null },
+);
+
+const appointmentId = "194414ff-46bf-4fd2-aa37-c58f3884f0de";
+console.log(`😀> Get appointment ${appointmentId}`);
+console.dir(await redox.get(`Appointment/${appointmentId}`), {
+  depth: null,
+});
